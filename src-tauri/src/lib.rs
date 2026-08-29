540b7fd2cd37f1f152401582f0f3798b141066aa
Martine Daigneault <martine@statis.ca>
2026-08-29 17:48:04 -0400

    Fix Supabase requests in Windows desktop app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,16 +7,12 @@ pub fn run() {
   std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
 
   tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
+    .plugin(tauri_plugin_http::init())
+    .plugin(
+      tauri_plugin_log::Builder::default()
+        .level(log::LevelFilter::Info)
+        .build(),
+    )
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
